8b309e4c88000cfc5b757da0a1ceb20997fe44e6
Yunhan Zhang <yunhanzhang@protonmail.com>
2021-05-01 22:27:56 -0500

    add

--- Projects/Project3/js/script.js
+++ Projects/Project3/js/script.js
@@ -16,6 +16,18 @@ function initMap() {
       map,
       title: "Shanghai Tower",
     });
+
+    const beachMarker2 = new google.maps.Marker({
+      position: { lat: 31.240272102137343, lng: 121.50087938662085 },
+      map,
+      title: "Oriental Pearl TV Tower",
+    });
+
+    const beachMarker3 = new google.maps.Marker({
+      position: { lat: 31.237087246352342, lng: 121.50209392762672 },
+      map,
+      title: "Shanghai Guojin Center",
+    });
   
     const content = '<div><h1>Shanghai Tower</h1><p>Shanghai Tower is a 128-story, 632-meter (2,073 ft)-tall megatall skyscraper in Lujiazui, Pudong, Shanghai.</p><footer>More information can be found at <a href="https://en.wikipedia.org/wiki/Shanghai_Tower">https://en.wikipedia.org/wiki/Shanghai_Tower</a></footer></div>';
     const contentWindow = new google.maps.InfoWindow({
